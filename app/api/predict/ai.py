b2f06da6c3000ef6cd824341e825e2015009fd14
Chathura J Ekanayake <chathuraje01@gmail.com>
2023-05-21 23:16:04 +0530

    change location of model and update api ip

--- app/api/predict/ai.py
+++ app/api/predict/ai.py
@@ -4,7 +4,7 @@ import pickle
 def predict_url(url):
     data_url = fnc.predict_url_type(url)
 
-    file_name = "app/api/predict/model.pkl"
+    file_name = "model.pkl"
     model_ = pickle.load(open(file_name, 'rb'))
 
     output = model_.predict(data_url)[0]
