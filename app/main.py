772ff8f83f5f7ae0da39ea2175f9c4d8b692f55d
Chathura J Ekanayake <chathuraje01@gmail.com>
2023-05-28 18:41:03 +0530

    remove nmap scanner from service

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from .api.domain_validator.routes import router as domain_validator
-from .api.nmap_scanner.routes import router as nmap_scanner
 
 app = FastAPI()
 app.include_router(domain_validator)
-app.include_router(nmap_scanner)
