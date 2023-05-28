9d8aa92af15f9ca77c9b868d21528a72944e7952
Chathura J Ekanayake <chathuraje01@gmail.com>
2023-05-28 17:10:52 +0530

    fix a bug: module name changes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from api.domain_validator.routes import router as domain_validator
-from api.nmap_scanner.routes import router as nmap_scanner
+from .api.domain_validator.routes import router as domain_validator
+from .api.nmap_scanner.routes import router as nmap_scanner
 
 app = FastAPI()
 app.include_router(domain_validator)
